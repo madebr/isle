2f84d665c17093fc750575a64ec08c5d60cbaa92
itsmattkc <itsmattkc@gmail.com>
2023-06-30 17:57:26 -0700

    clarify wording

--- LEGO1/compat.h
+++ LEGO1/compat.h
@@ -1,7 +1,7 @@
 #ifndef ISLECOMPAT_H
 #define ISLECOMPAT_H
 
-// Various defines to enable compiling on other platforms.
+// Various macros to enable compiling with other/newer compilers.
 
 // Use `COMPAT_CONST` where something ought to be 'const', and a newer compiler would complain if it
 // wasn't, but we know it isn't 'const' in the original code.
